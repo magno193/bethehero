2f62232d7722dcb3de1b9ca112ed9a7aabd2d19a
Alexandre Ferreira <alexmlf93@gmail.com>
2020-03-25 21:16:31 -0300

    Estrutura da página Logon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,11 @@
 import React, { useState } from "react";
-import Header from "./Header";
+import Logon from "./pages/Logon";
+import './global.css'
 
 function App() {
-  let [counter, setCounter] = useState(0); // retorna [valor, funcaoDeAtualizacao]
-
-  function increment() {
-    setCounter(counter + 1);
-  }
 
   return (
-    <div>
-      <Header>Contador: {counter}</Header>
-      <button onClick={increment}>Incrementar</button>
-    </div>
+    <Logon />
   );
 }
 
---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { FiLogIn } from 'react-icons/fi';
+
+import './styles.css'
+import heroisImg from '../../assets/heroes.png'
+import logoImg from '../../assets/logo.svg'
+
+
+export default function Logon(props) {
+    return(
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="logo"/>
+
+                <form action="">
+                    <h1>Faça seu logon</h1>
+
+                    <input type="text" placeholder="Sua ID"/>
+                    <button>Entrar</button>
+
+                    <a href="/register">
+                        <FiLogIn size={16} color="#E02041"/>
+                        Não tenho cadastro
+                    </a>
+                </form>
+            </section>
+
+            <img src={heroisImg} alt="Heroes" />
+        </div>
+    );
+}
