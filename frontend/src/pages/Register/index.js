9168a942ffbaa01770f981205fac1a26c990fdec
Alexandre Ferreira <alexmlf93@gmail.com>
2020-03-26 07:19:01 -0300

    Cadastro de ONGS funcionando

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,38 +1,96 @@
-import React from "react";
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 
-import './styles.css';
-import logoImg from '../../assets/logo.svg'
+import api from "../../services/api";
+import "./styles.css";
+import logoImg from "../../assets/logo.svg";
 
-export default function Register(){
-    return(
-        <div className="register-container">
-            <div className="content">
-                <section>
-                    <img src={logoImg} alt="Be the hero"/>
-                    <h1>Cadastro</h1>
-                    <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG</p>
-                
-                    <Link className="back-link" to="/">
-                        <FiArrowLeft size={16} color="#E02041"/>
-                        Voltar para logon
-                    </Link>
-                </section>
+export default function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [whatsapp, setWhatsapp] = useState("");
+  const [city, setCity] = useState("");
+  const [uf, setUf] = useState("");
 
-                <form action="">
-                    <input type="text" placeholder="Nome da ONG"/>
-                    <input type="email" placeholder="E-Mail"/>
-                    <input type="text" placeholder="WhatsApp"/>
+  const history = useHistory();
 
-                    <div className="input-group">
-                        <input type="text" placeholder="Cidade"/>
-                        <input type="text" placeholder="UF" style={{ width: 80}}/>
-                    </div>
+  async function handleRegister(event) {
+    event.preventDefault();
 
-                    <button className="button" type="submit">Cadastrar</button>
-                </form>
-            </div>
-        </div>
-    );
-};
+    const data = {
+      name,
+      email,
+      whatsapp,
+      city,
+      uf
+    };
+
+    try {
+      const response = await api.post("ongs", data);
+
+      alert(`Seu ID de acesso: ${response.data.id}`);
+
+      history.push('/');
+    } catch (err) {
+      alert("Erro no cadastro, tente novamente.");
+    }
+  }
+
+  return (
+    <div className="register-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Be the hero" />
+          <h1>Cadastro</h1>
+          <p>
+            Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem
+            os casos da sua ONG
+          </p>
+
+          <Link className="back-link" to="/">
+            <FiArrowLeft size={16} color="#E02041" />
+            Voltar para logon
+          </Link>
+        </section>
+
+        <form onSubmit={handleRegister}>
+          <input
+            placeholder="Nome da ONG"
+            value={name}
+            onChange={event => setName(event.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="E-mail"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
+          <input
+            placeholder="Whatsapp"
+            value={whatsapp}
+            onChange={event => setWhatsapp(event.target.value)}
+          />
+
+          <div className="input-group">
+            <input
+              placeholder="Cidade"
+              value={city}
+              onChange={event => setCity(event.target.value)}
+            />
+            <input
+              placeholder="UF"
+              style={{ width: 80 }}
+              value={uf}
+              onChange={event => setUf(event.target.value)}
+            />
+          </div>
+
+          <button className="button" type="submit">
+            Cadastrar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
