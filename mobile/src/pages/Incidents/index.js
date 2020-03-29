1139bcc5cc98310e07b8c4477b42a153a9571f6f
Alexandre Ferreira <alexmlf93@gmail.com>
2020-03-29 07:28:36 -0300

    Estrutura e estilização da lista de casos

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 import logoImg from '../../assets/logo.png';
@@ -14,10 +15,65 @@ export default function Incidents(){
                     Total de <Text style={styles.headerTextBold}>0 casos</Text>
                 </Text>
             </View>
-            
+
             <View>
                 <Text style={styles.title}>Bem-vindo!</Text>
                 <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia!</Text>
+
+                <View style={styles.incidentList}>
+
+                    <View style={styles.incident}>
+                        <Text style={styles.incidentProperty}>ONG:</Text>
+                        <Text style={styles.incidentValue}>APAD</Text>
+
+                        <Text style={styles.incidentProperty}>CASO:</Text>
+                        <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+
+                        <Text style={styles.incidentProperty}>VALOR:</Text>
+                        <Text style={styles.incidentValue}>R$ 120,00</Text>
+
+                        <TouchableOpacity style={styles.detailsButton} onPress={() => {}}>
+                            <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+                            <Feather name="arrow-right" size={16} color="#e02041"/>
+                        </TouchableOpacity>
+                    </View>
+
+                    <View style={styles.incident}>
+                        <Text style={styles.incidentProperty}>ONG:</Text>
+                        <Text style={styles.incidentValue}>APAD</Text>
+
+                        <Text style={styles.incidentProperty}>CASO:</Text>
+                        <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+
+                        <Text style={styles.incidentProperty}>VALOR:</Text>
+                        <Text style={styles.incidentValue}>R$ 120,00</Text>
+
+                        <TouchableOpacity style={styles.detailsButton} onPress={() => {}}>
+                            <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+                            <Feather name="arrow-right" size={16} color="#e02041"/>
+                        </TouchableOpacity>
+
+                    </View>
+
+                    <View style={styles.incident}>
+                        <Text style={styles.incidentProperty}>ONG:</Text>
+                        <Text style={styles.incidentValue}>APAD</Text>
+
+                        <Text style={styles.incidentProperty}>CASO:</Text>
+                        <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+
+                        <Text style={styles.incidentProperty}>VALOR:</Text>
+                        <Text style={styles.incidentValue}>R$ 120,00</Text>
+
+                        <TouchableOpacity style={styles.detailsButton} onPress={() => {}}>
+                            <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+                            <Feather name="arrow-right" size={16} color="#e02041"/>
+                        </TouchableOpacity>
+
+                    </View>
+
+                </View>
+
             </View>
 
         </View>
--- mobile/src/pages/Incidents/styles.js
+++ mobile/src/pages/Incidents/styles.js
@@ -31,10 +31,45 @@ export default StyleSheet.create({
         fontWeight: 'bold'
     },
 
-
     description: {
         fontSize: 16,
         lineHeight: 24,
         color: '#737380'
+    },
+
+    incidentList: {
+        marginTop: 32
+    },
+
+    incident: {
+        padding: 24,
+        borderRadius: 10,
+        backgroundColor: '#fff',
+        marginBottom: 16
+    },
+
+    incidentProperty: {
+        fontSize: 14,
+        color: '#41414d',
+        fontWeight: 'bold'
+    },
+
+    incidentValue: {
+        marginTop: 8,
+        fontSize: 15,
+        marginBottom: 24,
+        color: '#737380'
+    },
+
+    detailsButton: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+
+    detailsButtonText: {
+        color: '#e02041',
+        fontSize: 15,
+        fontWeight: 'bold'
     }
 });
