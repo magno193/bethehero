3ec81603e346ae65d31628cbc9742e08b0625b09
Alexandre Ferreira <alexmlf93@gmail.com>
2020-03-29 06:53:01 -0300

    Estilização inicial de Incidents

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image, Text } from 'react-native';
+
+import styles from './styles';
+import logoImg from '../../assets/logo.png';
 
 export default function Incidents(){
     return(
-        <View />
+        <View style={styles.container}>
+
+            <View style={styles.header}>
+                <Image source={logoImg}/>
+                <Text style={styles.headerText}>
+                    Total de <Text style={styles.headerTextBold}>0 casos</Text>
+                </Text>
+            </View>
+            
+            <View>
+                <Text style={styles.title}>Bem-vindo!</Text>
+                <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia!</Text>
+            </View>
+
+        </View>
     );
 }
